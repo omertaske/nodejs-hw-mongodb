a0a963855f5920659b4d52892ad174969a83cbeb
Ömer Task <omertaskesenn@gmail.com>
2025-09-22 14:25:56 +0300

    sa

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,5 +1,7 @@
 // src/utils/cloudinary.js
 import { v2 as cloudinary } from "cloudinary";
+import { config } from "dotenv";
+config(); 
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
